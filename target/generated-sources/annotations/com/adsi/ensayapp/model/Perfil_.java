89fc585e1e57968f4c9c7f1d86c8579a2b02bc6f
Felipe <felipegarxon@hotmail.com>
2020-10-05 20:26:42 -0500

    Add export to excel

--- target/generated-sources/annotations/com/adsi/ensayapp/model/Perfil_.java
+++ target/generated-sources/annotations/com/adsi/ensayapp/model/Perfil_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-29T17:47:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-05T19:47:08")
 @StaticMetamodel(Perfil.class)
 public class Perfil_ { 
 
