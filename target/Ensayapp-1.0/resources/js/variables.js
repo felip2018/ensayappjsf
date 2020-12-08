0c129eb62c46bd129d0179900049789dc7600edc
Felipe <felipegarxon@hotmail.com>
2020-12-08 15:02:38 -0500

    Remove target folder

--- target/Ensayapp-1.0/resources/js/variables.js
+++
@@ -1,4 +0,0 @@
-var emailRegex =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-var jsonMenuURL = 'http://localhost/ensayapp/resources/menu.json';
-var perfil;
-
